94c39459cd634d20310f07eb51bb60d869a0a94a
Daniil Dolotkazin <daniilkadm@gmail.com>
2019-09-01 21:40:33 +0300

    graphql settings update and refactoring

--- app.js
+++ app.js
@@ -1,72 +1,26 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const graphqlHttp = require('express-graphql');
-const { buildSchema } = require('graphql');
-const mongoose = require('mongoose');
+const express = require('express')
+const bodyParser = require('body-parser')
+const graphqlHttp = require('express-graphql')
+const mongoose = require('mongoose')
 
-const PORT = 3000;
+const graphQlSchema = require('./graphql/schema/index')
+const graphQlResolvers = require('./graphql/resolvers/index')
+
+const PORT = 3000
 
 const app = express()
 
-const events = []
-
 app.use(bodyParser.json())
 
+
+
 app.use(
-  '/graphql', 
+  '/graphql',
   graphqlHttp({
 
-    schema: buildSchema(`
-      type Event {
-        _id: ID!
-        title: String!
-        description: String!
-        price: Float!
-        date: String!
-      }
+    schema: graphQlSchema,
 
-      input EventInput {
-        title: String!
-        description: String!
-        price: Float!
-        date: String!
-      }
-
-      type RootQuery {
-        events: [Event!]!
-      }
-
-      type RootMutation {
-        createEvent(eventInput: EventInput): Event
-      }
-
-      schema {
-        query: RootQuery
-        mutation: RootMutation
-      }
-    `),
-
-    rootValue: {
-
-      events: () => {
-        return events
-      },
-
-      createEvent: (args) => {
-        const event = {
-          _id: Math.random().toString(),
-          title: args.eventInput.title,
-          description: args.eventInput.description,
-          price: +args.eventInput.price,
-          date: args.eventInput.date
-        };
-
-        events.push(event)
-
-        return event
-   
-      }
-    },
+    rootValue: graphQlResolvers,
 
     graphiql: true
   })
@@ -74,7 +28,9 @@ app.use(
 
 
 mongoose.connect(
-  `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@ddaniil-cluster-vas1i.mongodb.net/test?retryWrites=true&w=majority`, { useNewUrlParser: true }
+  `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@ddaniil-cluster-vas1i.mongodb.net/${process.env.MONGO_DB}?retryWrites=true&w=majority`, {
+    useNewUrlParser: true
+  }
 ).then(() => {
   app.listen(PORT, () => {
     console.log(`Running the app on port: ${PORT}`)
@@ -82,5 +38,3 @@ mongoose.connect(
 }).catch(err => {
   console.log(err)
 })
-
-
