c00b4695e9c4ab92e9f5eea3f78c26d58f1512fb
Daniil Dolotkazin <daniilkadm@gmail.com>
2019-09-03 21:52:40 +0300

    added some frontend

---
+++ frontend/src/pages/Booking.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class BookingsPage extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>The Bookings Page</h1>
+      </div>
+    )
+  }
+}
---
+++ frontend/src/pages/Events.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class EventsPage extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>The Events Page</h1>
+      </div>
+    )
+  }
+}
