24573c3798d815c5149f220796e5cb725f650274
Daniil Dolotkazin <daniilkadm@gmail.com>
2019-09-04 18:17:39 +0300

    auth frontend settings

--- app.js
+++ app.js
@@ -7,11 +7,21 @@ const graphQlSchema = require('./graphql/schema/index')
 const graphQlResolvers = require('./graphql/resolvers/index')
 const isAuth = require('./middleware/is-auth')
 
-const PORT = 3000
+const PORT = 8000
 
 const app = express()
 
 app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'POST,GET,OPTIONS')
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200)
+  }
+  next()
+})
 
 app.use(isAuth)
 
